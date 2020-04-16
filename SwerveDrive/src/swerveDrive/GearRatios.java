c87b85ebd4d7fbdd02da801f90311763c2dbc980
Radagast-the-infinite <ng.kyle101@gmail.com@gmail.com>
2020-04-16 00:30:55 -0700

    Made a new Gear Ratios class. Working on using the swerve unit class to create the gear objects now.

---
+++ SwerveDrive/src/swerveDrive/GearRatios.java
@@ -0,0 +1,17 @@
+package swerveDrive;
+
+import java.util.ArrayList;
+
+public class GearRatios {
+	// given two values in a list, [# of input gear, # of output gear]
+	public static double getRatio(int inputGearTeeth, int outputGearTeeth) {
+		double gearRatio = inputGearTeeth/outputGearTeeth;
+		return gearRatio;
+	}
+	 
+	// calculate conversions for gear ratios
+	public static ArrayList<Double> getFinalGearRatio() {
+		
+	return null;			
+	}
+}
